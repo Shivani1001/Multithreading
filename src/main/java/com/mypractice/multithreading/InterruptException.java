fb69cb41c60e177db5170ebce2daee2fd984d4f7
Shivani Sahu <Shivani_Sahu@gap.com>
2025-02-12 11:22:13 +0530

    Thread communication

---
+++ src/main/java/com/mypractice/multithreading/InterruptException.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mypractice.multithreading;
+
+/**
+ *
+ * @author shsahu
+ */
+class InterruptException {
+    
+}
---
+++ src/main/java/com/mypractice/multithreading/ThreadCommunication.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mypractice.multithreading;
+
+/**
+ *
+ * @author shsahu
+ */
+
+class SharedResource{
+    private int data;
+    private boolean hasData;
+    
+    public synchronized void produce(int value){
+    while(hasData){
+    
+        try{
+            wait();
+        } catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+            }
+        }
+    data = value;
+    hasData = true;
+    System.out.println("Produced:" + value);
+    notify();
+    }
+    
+    public synchronized int consume(){   
+        while(!hasData){
+        try{
+            wait();
+        } catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+            }
+        }
+        hasData = false;
+        System.out.println("Consumed:" + data);
+        notify();
+        return data;
+    }
+}
+    
+    class Producer implements Runnable{
+    private SharedResource resource;
+
+    public Producer(SharedResource resource) {
+        this.resource = resource;
+    }
+    
+    @Override
+    public void run(){
+        for(int i = 0; i<10;i++){
+            resource.produce(i);
+            
+          }
+        }
+    }
+
+    class Consumer implements Runnable{
+        private SharedResource resource;
+
+    public Consumer(SharedResource resource) {
+        this.resource = resource;
+    }
+    
+    @Override
+    public void run(){
+        for(int i = 0; i<10;i++){
+            int value = resource.consume();
+            
+          }
+        }
+    }
+
+
+public class ThreadCommunication {
+    public static void main(String[] Args){
+    SharedResource resource = new SharedResource();
+    Thread producerThread = new Thread(new Producer(resource));
+    Thread consumerThread = new Thread(new Consumer(resource));
+    
+    producerThread.start();
+    consumerThread.start();
+    }
+}
